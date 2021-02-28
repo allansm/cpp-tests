cebef1dcfe93cb9c7aa30866f9784fdd11f4103f
Allan De Souza Melo <allan_de_souza_melo@hotmail.com>
2021-02-28 20:03:22 -0300

    ...

--- autoytdl.cpp
+++ autoytdl.cpp
@@ -8,9 +8,21 @@
 
 #include <sys/time.h>
 #include <windows.h>
+#include <algorithm>
+#include <cctype>
 
 using namespace std;
 
+
+void overrideFile(string data,string fileName){
+    string command = "echo "+data+" > "+fileName;
+    system(command.c_str());
+}
+
+void addLineToFile(string data,string fileName){
+    string command = "echo "+data+" >> "+fileName;
+    system(command.c_str());
+}
 
 int countLines(const char* file){
     string text;
@@ -45,6 +57,40 @@ string getRandomLine(const char* file){
     return lines[iSecret];
 }
 
+string removeBlank(string txt){
+    string text = txt;
+    string delimiter = " ";
+    text.replace(text.find(delimiter),text.find(delimiter)+delimiter.length(),"");
+
+    return text;
+}
+
+void removeLine(const char* file,string line){
+    ifstream temp(file);
+    string text;
+    int size = countLines(file);
+    string lines[size];
+    int count = 0;
+    string txt = "";
+    //line = removeBlank(line);
+    while (getline (temp, text)) {
+      //text = removeBlank(text);
+      if(text != line){
+        //addLineToFile(text,file);
+        txt += text+"\n";
+        //cout << "text:" << text << "line:" << line << "\n";
+      }
+    }
+    remove(file);
+    ofstream output(file);
+    output.write(txt.c_str(),txt.length());
+    output.close();
+    temp.close();
+    //overrideFile("null",file);
+    //overrideFile(txt,file);
+}
+
+
 string getFirstLine(const char* file){
     string line;
     ifstream temp(file);
@@ -55,10 +101,7 @@ string getFirstLine(const char* file){
     return "";
 }
 
-void overrideFile(string data,string fileName){
-    string command = "echo "+data+" > "+fileName;
-    system(command.c_str());
-}
+
 
 bool generate(const char* file){
     if(getFirstLine(file) == "0"){
@@ -111,6 +154,9 @@ void run(){
             cout << "removing files\n";
 
             string link = getRandomLine("links.txt");
+            removeLine("links.txt",link);
+            cout << "remove link :" << link << "\n";
+
             string command = "youtube-dl.exe "+link;
 
             system(command.c_str());
