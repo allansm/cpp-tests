de149931b4a925eec27ece67ed20f1857c376bb7
Allan De Souza Melo <37338692+allansm@users.noreply.github.com>
2021-03-19 13:57:50 -0300

    ...

--- loopme.cpp
+++ loopme.cpp
@@ -1,13 +1,29 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <iostream>
-#include <windows.h>
+#include "class/io.h"
+#include "class/files.h"
+#include "class/util.h"
 
 using namespace std;
 
-main(){
-	string cmd = "ffplay -an -x 360 -y 200 -left 0 -top 0 -alwaysontop -noborder -autoexit http://localhost/stream/test2";
-	//cin >> cmd;
+int main(int argc, char* argv[]){
+	int n;
+	cout << "line:";
+	cin >> n;
+	string appname = argv[0];
+	while(true){
+		if(appname.find("\\") != -1){
+			int i = appname.find("\\");
+			i = (i==0)?1:i;
+			appname.erase(0,i);
+			cout << appname << endl;
+		}else{
+			break;
+		}
+	}
+	appname = appname.substr(0,appname.find("."));
+	string file = appname+".txt";
+	cout << file << endl;
+	string cmd = Files::getLine(file.c_str(),n);
+	
 	while(true){
 		system(cmd.c_str());
 		Sleep(1);
