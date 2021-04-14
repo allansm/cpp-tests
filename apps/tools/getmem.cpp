f894d32acc0e454300230d7cca0f1719679cef1e
allansm <allan.de.souza.melo.rj@gmail.com>
2021-04-14 17:16:30 -0300

    ...

---
+++ apps/tools/getmem.cpp
@@ -0,0 +1,42 @@
+#include "class/io.h"
+#include "class/files.h"
+#include "class/util.h"
+#include <string.h>
+
+string removeBlank(string str){
+	int size = str.length();
+	string ret = "";
+	for(int i=0;i<size;i++){
+		if(str.at(i) != ' '){
+			ret+=str.at(i);
+		}
+	}
+	return ret;
+}
+
+string getDigits(string str){
+	int size = str.length();
+	string ret = "";
+	for(int i=0;i<size;i++){
+		if(isdigit(str.at(i))){
+			ret+=str.at(i);
+		}
+	}
+	return ret;
+}
+
+float getAvaibleMemory(){
+	string cmd = "wmic OS get FreePhysicalMemory > avaible.txt";
+	system(cmd.c_str());
+	string avaible = Files::getLines("avaible.txt")[1];
+	avaible = getDigits(avaible);
+	return Util::parseFloat(avaible)/1000;
+}
+
+float getRam(){
+	return Util::parseFloat(Files::getFirstLine("ram.txt"));
+}
+
+main(){
+	cout << getAvaibleMemory();	
+}
