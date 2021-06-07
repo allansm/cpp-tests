a8433d37ffa0f960baf3bd075867ac1de6bd4d23
allansm <allan.de.souza.melo.rj@gmail.com>
2021-06-06 22:33:34 -0300

    ...

--- apps/tools/topmostConsole.cpp
+++ apps/tools/topmostConsole.cpp
@@ -1,17 +1,12 @@
-#include "../class/io.h"
+#include "class/io.h"
 #include <windows.h>
 
 void topmostConsole(){
 	HWND wnd;
-	wnd = FindWindowA("ConsoleWindowClass", NULL);
+	wnd = GetConsoleWindow();
 	SetWindowPos(wnd, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
 }
 
 main(){
-	/*
-	HWND wnd;
-	wnd = FindWindowA("ConsoleWindowClass", NULL);
-	SetWindowPos(wnd, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
-	*/
 	topmostConsole();
 }
