87cbd159dafffdd36295dbef3f1b615656de62c5
allansm <allan.de.souza.melo.rj@gmail.com>
2021-06-22 22:11:56 -0300

    ...

--- test.cpp
+++ test.cpp
@@ -89,15 +89,3 @@ string replace(string txt,string oldtxt,string newtxt){
 }
 
 
-main(){
-	int test = 0;
-
-	string* arr = new string[2];
-	arr[0] = "a";
-	arr[1] = "b";
-	arr[2] = "c";
-	
-	for(auto test:arr){
-		print(test);
-	}
-}
