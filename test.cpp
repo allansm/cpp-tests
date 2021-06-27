475c77f31eb533f9eb173f7142357c9ff0aa29e9
allansm <allan.de.souza.melo.rj@gmail.com>
2021-06-26 22:55:49 -0300

    ...

--- test.cpp
+++ test.cpp
@@ -1,7 +1,14 @@
 #include "class/io.h"
 #include "optimizedclass/util.h"
+#include "optimizedclass/time.h"
 #include <windows.h>
 
 main(){
-		
+	Time time;
+	Util util;
+	long int start = time.currentTimeToMs();
+	while(true){
+		system("cls");
+		util.println(time.elapsedTime(time.toSec(time.elapsedTime(start))));
+	}
 }
