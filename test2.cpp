06130ddae7a643cb08c079ff35d5d60b6647bc91
allansm <allan.de.souza.melo.rj@gmail.com>
2021-08-10 14:12:28 -0300

    ...

--- test2.cpp
+++ test2.cpp
@@ -1,8 +1,26 @@
 #include <wx/wxprec.h>
-#include <wx/wx.h>
+#ifndef WX_PRECOMP
+	#include <wx/wx.h>
+#endif
+class App : public wxApp{
+	public:
+		virtual bool OnInit();
+};
+class Frm : public wxFrame{
+	public:
+		Frm(const wxString& title, const wxPoint& pos, const wxSize& size);
+};
 
-main(){
-	//wxFrame *frame = 
-	wxFrame().wxFrame(wxString(""),wxPoint(50,50),wxSize(800,600))->Show(true);
-	//frame->Show(true);
+wxIMPLEMENT_APP(App);
+
+bool App::OnInit(){
+	Frm *frame = new Frm(wxString(""),wxPoint(50,50),wxSize(800,600));
+	frame->Show(true);
+	return true;
 }
+Frm::Frm(const wxString& title, const wxPoint& pos, const wxSize& size)
+	:wxFrame(NULL,wxID_ANY,title,pos,size){
+	
+}
+
+
