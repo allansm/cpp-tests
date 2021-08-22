6e8dfe9b8560fd0ea914d8088697a65cd2da516f
allansm <allan.de.souza.melo.rj@gmail.com>
2021-08-22 15:36:44 -0300

    ...

--- apps/tools/maximize.cpp
+++ apps/tools/maximize.cpp
@@ -1,14 +1,18 @@
 #include <class/io.h>
 #include <windows.h>
 
-main(){
-	HWND wnd;
-	wnd = GetConsoleWindow();
+void maximize(HWND wnd){
 	LONG lStyle = GetWindowLong(wnd, GWL_STYLE);
-	//lStyle &= ~(SW_MAXIMIZE);//~(WS_CAPTION | WS_THICKFRAME | WS_MINIMIZEBOX | WS_MAXIMIZEBOX | WS_SYSMENU);
 	lStyle &= ~WS_BORDER;
 	lStyle &= ~WS_DLGFRAME;
 	lStyle &= ~WS_THICKFRAME;
 	SetWindowLong(wnd, GWL_STYLE, lStyle);
 	ShowWindow(wnd,SW_SHOWMAXIMIZED);
+}
+
+main(){
+	HWND wnd;
+	wnd = GetConsoleWindow();
+	
+	maximize(wnd);
 }
