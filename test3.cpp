1b771d17a281dc4a9384a02df4f0420de46b6dd1
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2021-09-28 17:45:09 -0300

    ...

--- test3.cpp
+++ test3.cpp
@@ -1,24 +1,90 @@
 #include <cpp-lib/util.hpp>
 #include <cpp-lib/output.hpp>
-//#include <vector>
+
+#include <map>
 
 using namespace std;
 
 Util util = Util();
 
-main(){
-	string json = "{\"text\":\"helloworld\",\"n\":0}";
-	
-	for(string s : util.explode(json,",")){
-		if(has(s,"{") && !has(s,"}")){
-			s = util.replace(s,"{","");
-		}else if(has(s,"}") && !has(s,"{")){
-			s = util.replace(s,"}","");	
-		}
-		
-		string field = util.replace(util.explode(s,":")[0],"\"","");
-		string val = util.explode(s,":")[1];	
+map<string,string> test(string json){
+	json = json.substr(0, json.size()-1); 
+	json = json.substr(1, json.size());
 
-		println(field+":"+val);
+	map<string,string> tmp;
+
+	for(string s : util.explode(json,",")){
+		string ex = "";
+		if(has(s,":{")){
+			ex = "{"+util.explode(s,":{")[1];	
+		}else{
+			if(has(s,"{") && !has(s,"}")){
+				s = util.replace(s,"{","");
+			}else if(has(s,"}") && !has(s,"{")){
+				s = util.replace(s,"}","");	
+			}
+		}
+
+		string field = util.replace(util.explode(s,":")[0],"\"","");
+		field = util.replace(field,"'","");
+
+		string val;
+		if(ex == ""){
+			val = util.replace(util.explode(s,":")[1],"\"","");
+			val = util.replace(val,"'","");
+		}else{
+			val = ex;
+		}
+
+		tmp[field] = val;
 	}
+	
+	return tmp;	
+}
+
+main(){
+	string json = "{'text':'helloworld',\"n\":0,\"son\":{\"a\":1},'arr':['a','b','c']}";
+	
+	/*json = json.substr(0, json.size()-1); 
+	json = json.substr(1, json.size());
+
+	map<string,string> tmp;
+
+	for(string s : util.explode(json,",")){
+		string ex = "";
+		if(has(s,":{")){
+			ex = "{"+util.explode(s,":{")[1];	
+		}else{
+			if(has(s,"{") && !has(s,"}")){
+				s = util.replace(s,"{","");
+			}else if(has(s,"}") && !has(s,"{")){
+				s = util.replace(s,"}","");	
+			}
+		}
+
+		string field = util.replace(util.explode(s,":")[0],"\"","");
+		field = util.replace(field,"'","");
+
+		string val;
+		if(ex == ""){
+			val = util.replace(util.explode(s,":")[1],"\"","");
+			val = util.replace(val,"'","");
+		}else{
+			val = ex;
+		}
+
+		tmp[field] = val;
+	}*/
+	
+	auto tmp = test(json);
+
+	println(tmp["text"]);
+	println(tmp["n"]);
+	println(tmp["son"]);
+	
+	println("");
+
+	auto tmp2 = test(tmp["son"]);
+
+	println("a:"+tmp2["a"]);
 }
