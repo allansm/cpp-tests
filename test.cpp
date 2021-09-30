f55e121c2771292796248b8eb19e00b44c27f845
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2021-09-29 22:30:39 -0300

    ...

--- test.cpp
+++ test.cpp
@@ -1,18 +1,48 @@
 #include <iostream>
 #include <map>
+#include <any>
 
 using namespace std;
 
-template<typename T>
-struct dat{
-	T test;
+//template<typename T>
+/*map<string,any> test(){
+	map<string,any> tmp;
+
+	tmp["val"] = 2;
+	tmp["txt"] = "helloworld";
+
+	return tmp;
+}*/
+
+/*T test(T x){
+	return x+y;
+}*/
+
+struct aa{
+	T val;	
 };
 
 main(){
-	dat<string> tes;
-	tes.test = "helloworld";
+	//any x = 2;
+	//
+	std::any test;
+	test = "helloworld";
 
-	cout << tes.test;
+	cout << test;
+
+	//cout << x.type().name();
+	/*dat<string> tes;
+	tes.test = "helloworld";
+	*/
+	/*aa<int> a;
+	a.val = 2;
+
+	map<string,aa<string>> ta;
+
+	ta["val"] = a;*/
+	
+//	map<string,any> arr = test();
+
 /*
 	map<string,dat> test;
 	test["val"] = t;
--- test3.cpp
+++ test3.cpp
@@ -10,60 +10,6 @@ using namespace std;
 
 Util util = Util();
 Files files = Files();
-/*map<string,string> test(string json){
-	json = json.substr(0, json.size()-1); 
-	json = json.substr(1, json.size());
-
-	map<string,string> tmp;
-	
-	while(util.has(json,":[")){
-		string temp = util.explode(json,":[")[1];
-		temp = util.explode(temp,",")[0];
-
-		temp = ":["+temp;
-		
-		temp = util.replace(temp,",","");
-
-		string last = "";
-		for(string n : util.explode(util.explode(json,temp)[0],",")){
-			last = n;
-		}
-
-		//arr.push_back(last+temp);
-		
-		json = util.replace(json,last+temp,"");
-
-		tmp[last] = util.replace(temp,":","");
-	}
-		
-	for(string s : util.explode(json,",")){
-		string ex = "";
-		if(has(s,":{")){
-			ex = "{"+util.explode(s,":{")[1];	
-		}else{
-			if(has(s,"{") && !has(s,"}")){
-				s = util.replace(s,"{","");
-			}else if(has(s,"}") && !has(s,"{")){
-				s = util.replace(s,"}","");	
-			}
-		}
-
-		string field = util.replace(util.explode(s,":")[0],"\"","");
-		field = util.replace(field,"'","");
-
-		string val;
-		if(ex == ""){
-			val = util.replace(util.explode(s,":")[1],"\"","");
-			val = util.replace(val,"'","");
-		}else{
-			val = ex;
-		}
-
-		tmp[field] = val;
-	}
-	
-	return tmp;	
-}*/
 
 map<string,string> test2(string json){
 	json = json.substr(0, json.size()-1); 
@@ -180,74 +126,6 @@ vector<string> test3(string jsonArray){
 	
 	return util.explode(jsonArray,",");
 }
-
-/*void test4(){
-	string json = "";
-	bool flag = false;
-	for(string n : files.getLines("../test.json")){
-		if(flag){
-			json+="\n";
-		}else{
-			flag = true;
-		}
-		json+=n;
-	}
-
-	//println(json);
-	
-
-	char buff[255];
-	bool start = false;
-
-	string field = "";
-	string val = "";
-	bool captureField = false;
-	bool captureVal = false;
-	int n = 0;
-	for(int i = 0; i < json.length();i++){
-		if(start){
-			if(captureField){
-				if(json.at(i) == '\"' or json.at(i) == '\''){
-					string txt = buff;
-					println(txt);
-					n = 0;
-					memset(buff,'\0',255);
-					captureField = false;
-				}else{
-					buff[n++] += json.at(i);
-				}
-			}else if(captureVal){
-				if(json.at(i) == ','){
-					n = 0;
-					memset(buff,'\0',255);
-					captureVal = false;	
-				}else{
-					buff[n++] += json.at(i);
-				}
-
-			}
-
-
-			if(json.at(i) == ':'){
-				if(!captureVal && !captureField){
-					captureVal = true;
-				}
-			}
-
-			if(json.at(i) == '\"' or json.at(i) == '\''){
-				if(!captureField && !captureVal){
-					captureField = true;
-					println("starting capture");
-				}
-			}
-
-		}
-		
-		if(json.at(i) == '{' && !start){
-			start = true;
-		}
-	}
-}*/
 
 main(){
 	//string json = "{'text':'helloworld',\"n\":0,\"son\":{\"a\":1,'message':'helloworld'},'arr':['a','b','c'],\"arr2\":['d','e']}";	
