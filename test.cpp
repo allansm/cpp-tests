193db3da4fa5429217a2ffed5a0574d7b1e82170
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2021-10-02 20:22:01 -0300

    ...

--- test.cpp
+++ test.cpp
@@ -4,42 +4,13 @@
 
 using namespace std;
 
-template<typename T>
-undefined unk(T x){
-	return undefined().get(x);
-}
-
-Array<undefined> test(){
+main(){
 	Array<undefined> arr;
 	
-	arr["x"] = unk(2);
-	arr["y"] = unk("2.25");
+	arr["a"] = unknown(255);
+	arr["b"] = unknown(2.5);
 	
-	return arr;
-}
+	arr["val"] = unknown(arr["a"].get<int>()+arr["b"].get<float>());
 
-template<typename T>
-T test2(string name){
-	auto arr = test();
-	
-	return arr[name].get<T>();
-}
-
-
-struct test{
-	test(){}
-
-	int operator()() const{
-		return 2;
-	}
-};
-
-main(){	
-	println(test2<int>("x")+test2<float>("y"));
-	
-	test t;
-
-	int val = t();
-
-	print(val);
+	println("val:"+arr["val"].get<string>());
 }
