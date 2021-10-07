7af0975059cbb38de79288c8921b776a15fdff48
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2021-10-07 17:19:14 -0300

    ...

--- testjson.cpp
+++ testjson.cpp
@@ -5,24 +5,22 @@
 Util util = Util();
 
 main(){
-	//string tmp = util.exec("curl -s https://api.mangadex.org/manga?title="+util.input("title:"));
-	
-	//tmp = Util().replace(tmp,"\\u","");	
-	string son = "{'a':'b','c':'d','e':[{'msg':'hello',numbers:['1','2']},{'a':['3','4']}],'f':[1,2],'g':{'h':'i'}}";
-	
-	/*Json t = Json(tmp);
-	Json t2 = Json(t.get<string>("data",0));
-	
-	println(t2.get<string>("id"));
+	string json = Files().readFile("../test.json");
 
-	println(t2.get<string>("type"));
-
-	println(t2.object("attributes").array("tags",0).object("attributes").get<string>("name"));
-	*/
-
-	Json j  = Json(son);
+	Json test = Json(json);
 	
-	string n1 = j.array("e",0).get<string>("numbers",0);
-	string n2 = j.array("e",0).get<string>("numbers",1); 
-	println(to<int>(n1)+to<int>(n2));
+	Json values1 = test.array("values",0);
+	Json values2 = test.array("values",1);
+	
+	println(test.toString());
+
+	println(values1.get<int>("x")+test.get<float>("values2","y"));	
+	
+	print_r(test.fields);
+
+	Json test2 = Json(test.toString());
+
+	println(test2.object("user").get<string>("username"));
+
+	print_r(test.array<string>("numbers"));
 }
