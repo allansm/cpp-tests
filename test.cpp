c0ebbe6e6a5efe1f959f0728abec69702f1e3746
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2021-10-08 21:58:54 -0300

    ...

--- test.cpp
+++ test.cpp
@@ -3,60 +3,11 @@
 
 using namespace std;
 
-string removeSpaces(string txt){
-	int chain = 0;
-	int chain2 = 0;
-	int chain3 = 0;
-	
-	char buffer[99999];
-	int  n = 0;
-	for(int i=0;i<txt.length();i++){
-		char cha = txt.at(i);
-		
-		if(cha == '{' || cha == '['){
-			//chain++;
-		}
-		if(cha == '"' && chain2 ==0){
-			chain2++;
-		}else if(cha == '"' && chain2 > 0){
-			chain2--;
-		}
-
-		if(cha == '\'' && chain3==0){
-			chain3++;
-		}else if(cha == '\'' && chain3 > 0){
-			chain3--;
-		}
-		
-		if(cha == '}' || cha == ']'){
-			//chain--;
-		}
-
-		if(cha == ' ' && chain == 0 && chain2 == 0 && chain3 == 0){
-			
-		}else{
-			buffer[n++]=cha;
-		}
-	}
-
-	string str = buffer;
-
-	return str;
-}
-
 main(){
-	/*auto tmp = Json("{'a':[1,2,3]}").array<int>("a");
-
-	print_r(tmp);
-
-	print(tmp[0]+tmp[2]);*/
-
-	auto test = Json("{'a' : 'b' , 'b' : 'hello world','c':2}");
-	
-	println(removeSpaces(test.data));
-
-	//println(test.data);
-	println(test.base["b"].get());
+	auto test = Json("{ 'a' : 'b' , 'b' : 'hello world', 'c' : 2 }");
+		
+	println(test.get("b"));
+	println(test.get<int>("c")+1);
 	string tes = "hello world";	
 	println(unknown(tes).get());
 
