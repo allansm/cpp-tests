878e53e7f460781101c6820e524b4da86db6da72
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2021-10-09 18:08:41 -0300

    ...

--- test.cpp
+++ test.cpp
@@ -1,16 +1,34 @@
 #include <cpp-lib/io.hpp>
 #include <cpp-lib/json.hpp>
+#include <cpp-lib/files.hpp>
 
 using namespace std;
 
 main(){
-	auto test = Json("{ 'a' : 'b' , 'b' : 'hello world', 'c' : 2 }");
-		
-	println(test.get("b"));
-	println(test.get<int>("c")+1);
-	string tes = "hello world";	
-	println(unknown(tes).get());
+	Json json = Json("test.json");
 
-	int n = unknown(2).get<int>();
-	println(unknown(2).get<int>()+unknown(2.25).get<float>());
+	println(json.get("msg"));
+	print_r(json.array<string>("letters"));
+
+	println(json.object("user").get("name")+" "+json.object("user").get("username"));
+	
+	println(json.array("values",0).get<int>("x")+json.array("values",1).get<float>("y"));
+	println(json.array("values",0).get<float>("y")+json.array("values",1).get<int>("x"));
+
+	println(json.object("values2").get<int>("x")+json.object("values2").get<float>("y"));
+
+	println("");	
+	println(json.get<int>("numbers",0)+json.get<int>("numbers",1));
+	println(json.get<int>("numbers",2)+json.get<int>("numbers",3));
+	println(json.get<int>("numbers",4)+json.get<int>("numbers",4));
+	
+	auto n = unknown(10.2);
+
+	println("n:"+n.get());
+	
+	n = unknown(n.get<float>()+2);
+
+	println("n+2:"+n.get());
+
+	println(Json("{'test':'works :D'}").get("test"));
 }
