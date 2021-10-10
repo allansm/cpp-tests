a0995d0c9a155efedc10d6706c933840b43b20d2
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2021-10-09 21:55:36 -0300

    ...

--- test.cpp
+++ test.cpp
@@ -1,17 +1,23 @@
 #include <cpp-lib/io.hpp>
-#include <cpp-lib/util2.hpp>
+#include <cpp-lib/util.hpp>
+#include <cpp-lib/files.hpp>
+#include <cpp-lib/parser.hpp>
 
 using namespace std;
 
 main(){
-	int val = input<int>("test:");	
+	int val = input<int>("n:");	
 	
-	string str = input("test2:");
-
-	println(val);
+	string str = input("file or array:");
+	
+	println(to<string>(val)+"+2 = "+to<string>(val+2));
 	println(str);
 
 	if(Util().has(str,";")){
 		print_r(Util().explode(str,";"));
+	}
+
+	if(Util().has(str,".")){
+		println(Files().readFile(str));
 	}
 }
