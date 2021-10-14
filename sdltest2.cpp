139063e2e9154786fe707b1468e6f6d8b5377c92
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2021-10-14 19:37:50 -0300

    ...

--- sdltest2.cpp
+++ sdltest2.cpp
@@ -3,10 +3,64 @@
 #include <SDL2/SDL_timer.h>
 
 #include <iostream>
-
+bool quit;
+int x = 0;
+int y = 0;
 void redsquare(SDL_Surface* screenSurface){
-	SDL_Rect rect = {50, 50, 100, 100};
+	SDL_Rect rect = {x, y, 100, 100};
 	SDL_FillRect(screenSurface, &rect, SDL_MapRGB(screenSurface->format, 0xFF, 0x00, 0x00));
+}
+
+void test(SDL_Window* win,SDL_Surface* screenSurface){
+	while( !quit ){
+		//std::cout << y;
+		SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+
+		redsquare(screenSurface);
+
+		SDL_UpdateWindowSurface(win);
+		// optionally your code comes here...
+
+		// The following inner loop flushes the event queue.
+		// (All events that have been queued up between two frames.)
+
+		// declare an event struct somewhere
+		SDL_Event event;
+
+		/* Poll for events */
+		while( SDL_PollEvent( &event ) ){   
+			switch( event.type ){
+		    	/* Keyboard event */
+		    	/* Pass the event data onto PrintKeyInfo() */
+				case SDL_KEYDOWN:
+					switch (event.key.keysym.scancode) {
+						case SDL_SCANCODE_UP:
+							y-=10;
+							break;	
+						case SDL_SCANCODE_DOWN:
+							y+=10;
+							break;
+					}
+					//y-=10;
+					break;
+				case SDL_KEYUP:
+					//y+=10;
+					break;
+			//PrintKeyInfo( &event.key );
+		     //   break;
+
+		    /* SDL_QUIT event (window close) */
+		    		case SDL_QUIT:
+					quit = 1;
+					break;
+
+		    //default:
+		      //  break;
+			}
+		}
+
+	    // optionally your code comes here...
+	}
 }
 
 int main(int argc, char *argv[])
@@ -23,14 +77,15 @@ int main(int argc, char *argv[])
 	
 
 	SDL_Surface* screenSurface = SDL_GetWindowSurface(win);
-  	SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+  	//SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
   
-	redsquare(screenSurface);
+	//redsquare(screenSurface);
 		
-    while(true){
+    /*while(true){
 	SDL_UpdateWindowSurface(win);
 	SDL_Delay(2000);
-    }
+    }*/
+	test(win,screenSurface);
  
     return 0;
 }
