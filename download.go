6905780b321dd8bf810519f5400b5d3523e1d925
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2021-10-21 19:01:50 -0300

    ...

--- download.go
+++ download.go
@@ -8,13 +8,19 @@ import "io/ioutil"
 func download(url *C.char) *C.char{
 	resp, err := http.Get(C.GoString(url))
 
+	if(err != nil){
+		return C.CString("");
+	}
+
 	defer resp.Body.Close()
 
 	html, err := ioutil.ReadAll(resp.Body)
 
-	str := string(html)
+	if(err != nil){
+		return C.CString("");
+	}
 
-	if(err == err){}
+	str := string(html)
 
 	return C.CString(str)
 }
--- gotest.cpp
+++ gotest.cpp
@@ -4,14 +4,41 @@
 #include <cpp-lib/json.hpp>
 
 using namespace std;
+struct json_array{
+	vector<json_obj> data;
+
+	json_array(string json){
+		//json = JsonHandle().removeSpaces(json);
+		println(json);
+		for(string tmp : JsonHandle().getArray(json)){
+			tmp = JsonHandle().removeSpaces(tmp);
+			this->data.push_back(json_obj(tmp));
+		}
+	}	
+};
 
 void out(){
-	string tmp = "http://worldtimeapi.org/api/timezone/America/Sao_Paulo";
+	string tmp = "http://mysafeinfo.com/api/data?list=englishmonarchs&format=json";
+	//string tmp = "http://worldtimeapi.org/api/timezone/America/Sao_Paulo";
 	string html = download(tmp);
-	Json json = Json(html);
+	
+	if(html == ""){
+		println("D:");
+	}else{
+		auto test = json_array(html);
+		
+		for(auto json : test.data){
+			/*for(auto s:json.fields){
+				println(s+":"+json.get(s));
+			}*/
+			println(json.data);	
+		}
 
-	for(auto s:json.fields){
-		println(s+":"+json.get(s));
+		/*Json json = Json(html);
+
+		for(auto s:json.fields){
+			println(s+":"+json.get(s));
+		}*/
 	}
 }
 
