ce8a6be11476bfb267e40c65ef30f725d2381e99
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2021-10-21 22:33:35 -0300

    ...

--- gotest.cpp
+++ gotest.cpp
@@ -4,49 +4,6 @@
 #include <cpp-lib/json.hpp>
 
 using namespace std;
-/*struct json_array{
-	vector<json_obj> data;
-
-	json_array(string json){	
-		json = Util().replace(json,"\t","");
-		json = Util().replace(json,"\r\n","");
-		json = JsonHandle().removeSpaces(json);
-
-		for(string tmp : JsonHandle().getArray(json)){
-			tmp = JsonHandle().removeSpaces(tmp);
-			this->data.push_back(json_obj(tmp));
-		}
-	}
-
-	operator vector<json_obj>(){
-		return this->data;
-	}
-};
-
-using JsonArray = json_array;*/
-
-string removeFormat(string json){
-	json = Util().replace(json,"\t","");
-	json = Util().replace(json,"\r\n","");
-	json = JsonHandle().removeSpaces(json);
-	
-	return json;
-}
-
-vector<json_obj> getArray(string json){
-	vector<json_obj> data;
-
-	json = Util().replace(json,"\t","");
-	json = Util().replace(json,"\r\n","");
-	json = JsonHandle().removeSpaces(json);
-
-	for(string tmp : JsonHandle().getArray(json)){
-		tmp = JsonHandle().removeSpaces(tmp);
-		data.push_back(json_obj(tmp));
-	}
-
-	return data;
-}
 
 void out(){
 	string tmp = "http://mysafeinfo.com/api/data?list=englishmonarchs&format=json";
@@ -56,23 +13,13 @@ void out(){
 	if(html == ""){
 		println("D:");
 	}else{	
-
-		/*println("--------------------------------------");	
-		for(auto json : getArray(html)){	
-			println(json.get("ID"));
-			println(json.get("Name"));
+		for(auto json : JsonArray(html)){	
 			println("--------------------------------------");
-		}*/
-		
-		/*println(getArray(html)[0].get("Name"));
-		println(getArray(html)[1].get("Name"));
-		print(getArray(html)[2].get("Name"));*/
-
-		string test = "{'array':"+removeFormat(html)+"}";
-
-		Json json = Json(test);
-
-		println(json.array("array",0).data);
+			for(auto f:json.fields){
+				println(f+":"+json.get(f));
+			}
+		}
+		print("--------------------------------------");	
 	}
 }
 
