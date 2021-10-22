98cab3cf9484430372ecb631c6fd83423a169b09
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2021-10-21 21:22:05 -0300

    ...

--- argtest.cpp
+++ argtest.cpp
@@ -1,51 +1,6 @@
 #include <iostream>
-//#include <stdlib.h>
-//#include <cpp-lib/util.hpp>
 #include <cpp-lib/path.hpp>
-/*std::string test(char **argv){
-	#if defined _WIN32
-		char buf[255] = "";
-		_fullpath(buf,argv[0],255);
-
-		return buf;
-	#elif defined (__LINUX__) || defined(__gnu_linux__) || defined(__linux__)
-		return realpath(argv[0],NULL);	
-	#endif
-
-	return "";
-}*/
-
-std::string test(char **argv){	
-	std::string tmp = argv[0];
-	
-	int end = 0;
-	for(int i = 0;i<tmp.length();i++){	
-		if(tmp.at(i) == '\\' || tmp.at(i) == '/'){
-			end = i;
-		}
-	}
-
-	auto relative = tmp.substr(0,end);
-
-	if(relative == ""){
-		return ".";
-	}else{
-		return relative;
-	}	
-}
-
-std::string test2(char **argv){
-	auto current = getcwd();
-
-	chdir(test(argv));
-
-	auto tmp = getcwd();
-
-	chdir(current);
-
-	return tmp;
-}
 
 main(int argc,char **argv){	
-	std::cout << test2(argv);
+	std::cout << selfLocation(argv);
 }
--- gotest.cpp
+++ gotest.cpp
@@ -4,18 +4,49 @@
 #include <cpp-lib/json.hpp>
 
 using namespace std;
-struct json_array{
+/*struct json_array{
 	vector<json_obj> data;
 
-	json_array(string json){
-		//json = JsonHandle().removeSpaces(json);
-		println(json);
+	json_array(string json){	
+		json = Util().replace(json,"\t","");
+		json = Util().replace(json,"\r\n","");
+		json = JsonHandle().removeSpaces(json);
+
 		for(string tmp : JsonHandle().getArray(json)){
 			tmp = JsonHandle().removeSpaces(tmp);
 			this->data.push_back(json_obj(tmp));
 		}
-	}	
+	}
+
+	operator vector<json_obj>(){
+		return this->data;
+	}
 };
+
+using JsonArray = json_array;*/
+
+string removeFormat(string json){
+	json = Util().replace(json,"\t","");
+	json = Util().replace(json,"\r\n","");
+	json = JsonHandle().removeSpaces(json);
+	
+	return json;
+}
+
+vector<json_obj> getArray(string json){
+	vector<json_obj> data;
+
+	json = Util().replace(json,"\t","");
+	json = Util().replace(json,"\r\n","");
+	json = JsonHandle().removeSpaces(json);
+
+	for(string tmp : JsonHandle().getArray(json)){
+		tmp = JsonHandle().removeSpaces(tmp);
+		data.push_back(json_obj(tmp));
+	}
+
+	return data;
+}
 
 void out(){
 	string tmp = "http://mysafeinfo.com/api/data?list=englishmonarchs&format=json";
@@ -24,21 +55,24 @@ void out(){
 	
 	if(html == ""){
 		println("D:");
-	}else{
-		auto test = json_array(html);
-		
-		for(auto json : test.data){
-			/*for(auto s:json.fields){
-				println(s+":"+json.get(s));
-			}*/
-			println(json.data);	
-		}
+	}else{	
 
-		/*Json json = Json(html);
-
-		for(auto s:json.fields){
-			println(s+":"+json.get(s));
+		/*println("--------------------------------------");	
+		for(auto json : getArray(html)){	
+			println(json.get("ID"));
+			println(json.get("Name"));
+			println("--------------------------------------");
 		}*/
+		
+		/*println(getArray(html)[0].get("Name"));
+		println(getArray(html)[1].get("Name"));
+		print(getArray(html)[2].get("Name"));*/
+
+		string test = "{'array':"+removeFormat(html)+"}";
+
+		Json json = Json(test);
+
+		println(json.array("array",0).data);
 	}
 }
 
