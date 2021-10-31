da81059387f0564434b33c31521bec5414d4620d
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2021-10-31 18:22:59 -0300

    ...

--- sdlmp3.cpp
+++ sdlmp3.cpp
@@ -1,21 +1,30 @@
-#include "SDL2/SDL.h"
-#include "SDL2/SDL_mixer.h"
-#include <iostream>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_mixer.h>
+#include <cpp-lib/io.hpp>
+#include <cpp-lib/path.hpp>
+
+void listen(const char* fn){
+	Mix_OpenAudio(22050, AUDIO_S16SYS, 2, 640);
+	Mix_Music *music = Mix_LoadMUS(fn);
+	Mix_PlayMusic(music, 1);
+
+	while (Mix_PlayingMusic()) {
+		SDL_Delay(250);
+	}
+
+	Mix_FreeMusic(music);
+	Mix_CloseAudio();
+}
 
 int main(int argc,char** argv){
-    std::string fn = argv[1];
+	chdir(argv[1]);
 
-    Mix_OpenAudio(22050, AUDIO_S16SYS, 2, 640);
-    Mix_Music *music = Mix_LoadMUS(fn.c_str());
-    Mix_PlayMusic(music, 1);
-
-    while (Mix_PlayingMusic()) {
-        SDL_Delay(250);
-    }
-
-    Mix_FreeMusic(music);
-    Mix_CloseAudio();
-    //SDL_Quit();
-
-    return 0;    
+	for(auto n : ls(".")){
+		if(n != "." && n != ".."){
+			println(n);
+			listen(n.c_str());
+		}
+	}
+	
+	return 0;    
 }
