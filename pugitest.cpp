17e970ca8cbb4bc08a252e075490df1d57ec3134
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2021-11-04 20:14:56 -0300

    ...

--- pugitest.cpp
+++ pugitest.cpp
@@ -1,114 +1,41 @@
-#include <pugixml.hpp>
-
-#include <cpp-lib/io.hpp>
-#include <cpp-lib/util.hpp>
-#include <cpp-lib/parser.hpp>
-
-pugi::xml_document _x_m_l;
-struct XmlNode{
-	pugi::xml_node data;
-	std::string name;
-	undefined value;
-	
-	XmlNode(pugi::xml_node data){
-		this->data = data;
-		this->value = unknown(data.child_value());
-		this->name = data.name();
-	}
-
-	XmlNode(std::string fn){	
-		_x_m_l.load_file(fn.c_str());
-		this->data = _x_m_l.root();
-		this->value = unknown(data.child_value());	
-		this->name = data.name();
-	}
-	
-	XmlNode get(std::string name){
-		return XmlNode(this->data.child(name.c_str()));
-	}	
-
-	
-	
-	vector<XmlNode> nodes(){
-		vector<XmlNode> tmp;
-		
-		for(auto n : this->data){
-			tmp.push_back(XmlNode(n));
-		}
-
-		return tmp;
-	}
-	
-	undefined var(std::string name){
-		return unknown(this->data.attribute(name.c_str()).value());
-	}
-	
-	XmlNode operator[](const int& i){
-		int c = 0;
-		for(auto n : this->data){
-			if(c++ == i){
-				return XmlNode(n);
-			}
-		}
-
-		return this->data;
-	}
-	
-	template <typename T>
-	undefined operator+(const T&a){
-		auto tmp = this->value;
-		tmp = unknown(tmp.get<T>()+a);
-
-		return tmp;
-	}
-	
-	template <typename T>
-	T operator+=(const T&a){
-		auto tmp = this->value;
-		tmp = unknown(tmp.get<T>()+a);
-
-		return tmp;
-	}
-		
-
-	/*template <typename T>
-	T operator+=(const T& b,const XmlNode& a){
-		auto tmp = this->value;
-		tmp = unknown(b+a.value.get<T>());
-
-		return tmp.get<T>();
-	}*/
-
-	template <typename T>
-	operator T(){
-		return this->value;
-	}
-
-	/*operator int(){
-		return this->value;
-	}
-
-	operator std::string(){
-		return this->value;
-	}
-
-	operator float(){
-		return this->value;
-	}
-
-	operator bool(){
-		return this->value;
-	}*/
-};
-
 #define useNode
 
-#ifdef useNode
-using node = XmlNode;
-#endif
+#include <cpp-lib/dependent/pugi.hpp>
 
-main(){		
-	node n = node("test.xml");
+class XmlDoc{
+	public:
+		pugi::xml_document data;
+
+		XmlDoc(std::string fn){
+			this->data.load_file(fn.c_str());
+		}
+
+		XmlNode root(){
+			return XmlNode(this->data.root());
+		}
+	
+		operator XmlNode(){
+			return this->root();
+		}
+};
+
+/*pugi::xml_node XmlDoc(std::string fn){
+	pugi::xml_document doc;
+	doc.load_file(fn.c_str());
+
+	return doc;
+}*/
+
+/*XmlNode test(std::string fn){
+	return rootNode(fn);
+}*/
+
+
+main(){	
+	//auto doc = XmlDoc("test.xml");
+	//pugi::xml_node tmp = doc.root();	
+	auto d = XmlDoc("test.xml");
+	node n = d;//d.root();//node(d.data.root());
 
 	int x = n.get("obj").get("x");
 	int y = n.get("obj").get("y");
@@ -121,8 +48,29 @@ main(){
 	println(n1);		
 	
 
-	node n2 = node("test.xml");
-	float res = n[0].get("x");
-	res +=(float)n[1].get("x");
-	println(res);	
+	//node n2 = node("test.xml");
+	float x1 = n[0].get("x");
+	float x2 = n[1].get("x");
+
+	float y1 = n[0].get("y");
+	float y2 = n[1].get("y");
+
+	println(x1+x2);
+	println(y1+y2);
+	
+	for(auto nd : n.nodes()){
+		println(nd.name);
+	}
+	
+	for(auto att : n[0].attributes()){
+		std::cout << att.name() << ":" << att.value() << std::endl;
+	}
+
+	print<std::string>(n.get("obj")[0]);	
+
+	/*auto test1 = rootNode("test.xml");
+	std::cout << test1.child("obj").name();*/
+	//auto test1 = test("test.xml");
+
+	//println(test1.name);
 }
