b9b900cd54f6049a7f37832136dea04993b257dd
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2021-11-19 16:43:57 -0300

    ...

--- shufflevector.cpp
+++ shufflevector.cpp
@@ -6,17 +6,29 @@
 #include <util.cpp>
 #include <time.cpp>
 
+template <typename T>
+T test(T arr){
+	unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+	std::default_random_engine e(seed);
+	std::shuffle(arr.begin(), arr.end(), e);
+
+	return arr;
+}
+
 main(){
+	std::vector<int> foo{1,2,3,4,5};
+	std::vector<std::string> foo2{"a","b","c","d"};
+	
 	while(true){
 		Time().sleep(200);
 		Util().clear();
 
-		unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-		std::default_random_engine e(seed);
+		foo = test(foo);
+		print_r(foo);
+		
+		println("");
 
-		std::vector<int> foo{1,2,3,4,5};
-		std::shuffle(foo.begin(), foo.end(), e);
-
-		print_r(foo);	
+		foo2 = test(foo2);
+		print_r(foo2);	
 	}
 }
