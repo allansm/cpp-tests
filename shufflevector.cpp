321273596edae4c0db245c190a0c3725d5cb7245
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2021-11-20 13:51:49 -0300

    ...

--- shufflevector.cpp
+++ shufflevector.cpp
@@ -1,19 +1,19 @@
-#include <iostream>
-#include <algorithm>
-#include <random>
-#include <chrono>
+//#include <iostream>
+//#include <algorithm>
+//#include <random>
+//#include <chrono>
 #include <allansm/io.hpp>
 #include <util.cpp>
 #include <time.cpp>
 
-template <typename T>
+/*template <typename T>
 T test(T arr){
 	unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
 	std::default_random_engine e(seed);
 	std::shuffle(arr.begin(), arr.end(), e);
 
 	return arr;
-}
+}*/
 
 main(){
 	std::vector<int> foo{1,2,3,4,5};
@@ -23,12 +23,12 @@ main(){
 		Time().sleep(200);
 		Util().clear();
 
-		foo = test(foo);
+		foo = Util().shuffle(foo);
 		print_r(foo);
 		
 		println("");
 
-		foo2 = test(foo2);
+		foo2 = Util().shuffle(foo2);
 		print_r(foo2);	
 	}
 }
