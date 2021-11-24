88f0974388112668c5a2de4861367ca8efd48f79
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2021-11-24 13:50:00 -0300

    ...

--- sdleventtest.cpp
+++ sdleventtest.cpp
@@ -10,7 +10,7 @@ struct Mouse{
 	void pos(SDL_Event e){
 		if(e.type == SDL_MOUSEMOTION){
 			SDL_GetGlobalMouseState(&this->x,&this->y);	
-		}	
+		}
 	}
 
 	void relativePos(SDL_Window * win,SDL_Event e){
@@ -21,7 +21,7 @@ struct Mouse{
 		
 			this->x -= x;
 			this->y -= y;
-		}	
+		}
 	}
 };
 
@@ -65,7 +65,7 @@ struct Rect{
 			if(mx >= x && mx <= x+w && my >= y && my <= y+h){
 				this->onclick();
 			}
-		}	
+		}
 	}
 
 	void click(SDL_Event e){
@@ -83,15 +83,67 @@ struct Rect{
 	}
 };
 
+struct test{
+	int x;
+	int y;
+	int w;
+	int h;
+	
+	int mx;
+	int my;
+
+	SDL_Window * window;
+	SDL_Event * event;
+	
+	void (*onclick)() = [](){};
+
+	operator SDL_Rect(){
+		SDL_Rect rect = {x,y,w,h};
+		
+		return rect;
+	}
+
+	void relativePos(SDL_Window * win,SDL_Event * e){
+		if(e->type == SDL_MOUSEMOTION){
+			int x,y;
+			SDL_GetGlobalMouseState(&this->mx,&this->my);	
+			SDL_GetWindowPosition(win,&x,&y);
+		
+			this->mx -= x;
+			this->my -= y;	
+		}
+	}
+
+	void click(){
+		this->relativePos(this->window,this->event);
+
+		if(this->event->type == SDL_MOUSEBUTTONDOWN){		
+			int x = this->x;
+			int y = this->y;
+			int w = this->w;
+			int h = this->h;
+				
+			if(this->mx >= x && this->mx <= x+w && this->my >= y && this->my <= y+h){
+				this->onclick();
+			}		
+		}
+	}
+	
+};
+
 Mouse mouse;
+
 Rect t2 = {0,0,100,100};
 Rect t3 = {200,0,50,50};
+test t4 = {0,200,100,40};
 
 int main(int argc,char ** argv){
 	sdl2 t;
 	
 	t.width = 800;
 	t.height = 600;
+	
+	t.start();
 
 	t2.mouse = &mouse;
 	t2.onclick = [](){
@@ -103,16 +155,21 @@ int main(int argc,char ** argv){
 		system("cls");
 	};
 
-	t.start();	
+	t4.window = t.window;
+	t4.event = &t.event;
+	t4.onclick = [](){
+		println("helloworld");
+	};
 	
 	t.onEvent = [](auto a){
 		mouse.relativePos(a->window,a->event);
 		t2.click(a->event);
 		t3.click(mouse.x,mouse.y,a->event);
+		t4.click();
 	};
 
 	t.onUpdate = [](auto a){
-		auto win = a->window;	
+		auto win = a->window;
 		
 		auto screenSurface = SDL_GetWindowSurface(win);
 		SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));	
@@ -123,12 +180,15 @@ int main(int argc,char ** argv){
 		rect = t3;
 		SDL_FillRect(screenSurface, &rect, SDL_MapRGB(screenSurface->format, 0x00, 0x00, 0xFF));
 		
+		rect = t4;
+		SDL_FillRect(screenSurface, &rect, SDL_MapRGB(screenSurface->format, 0x00, 0xFF, 0x00));
+		
 		rect = {mouse.x,mouse.y,5,5};
 		SDL_FillRect(screenSurface, &rect, SDL_MapRGB(screenSurface->format, 0x00, 0xFF, 0x00));
-		SDL_UpdateWindowSurface(win);	
+		SDL_UpdateWindowSurface(win);
 	};
 	
-	t.loop();	
+	t.loop();
 
 	return 0;
 }
