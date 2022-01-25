536b9a00ab875f17fff3b1d7349c69a3aafb901c
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2022-01-25 15:05:12 -0300

    ...

--- hwndtest/test.cpp
+++ hwndtest/test.cpp
@@ -1,6 +1,22 @@
-#include <windows.h>
+#include <allansm/win.hpp>
+#include <allansm/io.hpp>
+#include <allansm/parser.hpp>
+#include <allansm/util.hpp>
 
-main(){
-	HWND hwnd = GetConsoleWindow();
-	if( hwnd != NULL ){ MoveWindow(hwnd ,0,0 ,400,400 ,TRUE); }
+main(int argc,char ** argv){
+
+	for(auto n:Win().getAll()){
+
+		if(Win().getAppname(n) == argv[1]){
+			std::string a = argv[2];
+			std::string b = argv[3];
+			
+			int x = unknown(a);
+			int y = unknown(b);
+			
+			Win().resize(n, x, y);
+			
+			println("change");
+		}
+	}
 }
