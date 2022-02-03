83ac6db2b4336c42687d805e293fa6649493f3bc
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2022-02-02 21:08:22 -0300

    ...

--- zlibtest/test.cpp
+++ zlibtest/test.cpp
@@ -1,9 +1,9 @@
 #include <zlib.h>
 #include <iostream>
+#include <allansm/io.hpp>
 
 main(){
-	char text[255] = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"; 
-
+	char text[255] = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
 	char compressed[255];
 
 	z_stream stream;
@@ -21,6 +21,24 @@ main(){
 	deflate(&stream, Z_FINISH);
 	deflateEnd(&stream);
 
-	std::cout << compressed;
+	println(compressed);
+
+	char decompressed[255];
+
+	stream.zalloc = Z_NULL;
+	stream.zfree = Z_NULL;
+	stream.opaque = Z_NULL;
+
+    	stream.avail_in = (uInt) strlen(compressed)+1;
+	stream.next_in = (Bytef *) compressed;
+
+    	stream.avail_out = (uInt) sizeof(decompressed);
+    	stream.next_out = (Bytef *) decompressed;
+     
+    	inflateInit(&stream);
+    	inflate(&stream, Z_NO_FLUSH);
+    	inflateEnd(&stream);
+     	
+	println(decompressed);
 }
 
