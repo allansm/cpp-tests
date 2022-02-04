56225f293ceb385a4708673774ff4d0a4fe0e072
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2022-02-04 17:36:58 -0300

    ...

--- wproctime/wproctime.cpp
+++ wproctime/wproctime.cpp
@@ -3,6 +3,9 @@
 #include <allansm/elapse.hpp>
 #include <allansm/util.hpp>
 #include <allansm/time.hpp>
+#include <allansm/path.hpp>
+#include <allansm/file.hpp>
+#include <allansm/parser.hpp>
 
 struct Process{
 	string name;
@@ -12,15 +15,27 @@ struct Process{
 main(){
 	using namespace std;
 	
+	const int MINUTE = 1000*60;
+
+	auto data = File("wproctime"+Time().getDate("")+".data");
 	auto w = Win();
 	
 	vector<Process> procs;
-	string current;
 	
 	int ms = 0;
 	int i = 0;
-	
+
 	Elapse e = Elapse();
+	Elapse save = Elapse();
+	
+	chdir(getTemp());
+
+	if(data.exists()){
+		for(auto n : data.lines()){
+			auto tmp = Util().explode(n,";");
+			procs.push_back({tmp[0],unknown(tmp[1])});
+		}
+	}
 
 	while(true){
 		Util().clear();
@@ -42,16 +57,27 @@ main(){
 			procs.push_back({name,0});
 		}
 
-		if(!(current == name)){
-			procs[i] = {name,procs[i].ms+e.elapsed()};
-			e = Elapse();
+		procs[i] = {name,procs[i].ms+e.elapsed()};
+		e = Elapse();
+		
+		if(save.elapsed() > MINUTE){
+			data.write("");
+			println("saving...");
 		}
-
+		
 		for(auto n : procs){
 			print(n.name);
 			print(" ");
 			print(Time().elapsedTime(n.ms/1000));
 			println("");
+			
+			if(save.elapsed() > MINUTE){
+				data.add(n.name+";"+to_string(n.ms)+"\n");
+			}
+		}
+
+		if(save.elapsed() > MINUTE){
+			save = Elapse();
 		}
 
 		Time().sleep(1100);
