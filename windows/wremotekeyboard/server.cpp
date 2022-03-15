4235f833b3493dd63819dddbe4e58dc36b09e6c7
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2022-03-15 17:15:07 -0300

    Add connection with server.

--- windows/wremotekeyboard/server.cpp
+++ windows/wremotekeyboard/server.cpp
@@ -8,8 +8,14 @@ main(){
 	println("starting...");
 	Socket().server(54321, [](auto sock){
 		while(true){
-			std::string recv = Socket().receive(sock, 9);
+			std::string recv = Socket().receive(sock, 2);
 			
+			if(recv.at(0) == 'H'){
+				down(recv.at(1));
+			}else{
+				up(recv.at(1));
+			}
+
 			print("receive:");
 			println(recv);
 		}
--- windows/wremotekeyboard/wremotekeyboard.cpp
+++ windows/wremotekeyboard/wremotekeyboard.cpp
@@ -8,6 +8,8 @@
 
 #include <thread>
 
+SOCKET _sock;
+
 struct key_info{
 	int key;
 	int target;
@@ -26,21 +28,42 @@ std::vector<key_info> getKeys(){
 void keyAction(key_info info){
 	Key(info.key, [](auto self){
 		if(self->holding){
-			down(self->target);
+			char c = self->target;
+			std::string msg = "H"+std::string(1,c);
+			
+			Socket().send(_sock, msg);
+
 			print("holding ");
 			println((char)self->key);
-			
 		}else{
-			up(self->target);
+			char c = self->target;
+			std::string msg = "R"+std::string(1,c);
+			
+			Socket().send(_sock, msg);
+			
 			print("release ");
 			println((char)self->key);
 		}
 	}, info.target);
 }
 
+
+
+void getConnection(){
+	Socket().client("127.0.0.1", 54321, [](auto sock){
+		_sock = sock;
+		
+		println("connected\n");
+
+		while(true){}
+	});
+}
+
 std::vector<key_info> keys;
 
 main(){
+	std::thread connection_thread(getConnection);
+
 	keys = getKeys();
 
 	std::vector<std::thread> threads;
