d5ac392e51f11ebbfd8169422c269e4089bf8a7e
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2022-07-03 13:01:57 -0300

    Rewrite

--- windows/wopacity/wfocusopacity.cpp
+++ windows/wopacity/wfocusopacity.cpp
@@ -1,25 +1,40 @@
 #include <allansm/win.hpp>
-#include <allansm/parser.hpp>
 #include <allansm/time.hpp>
+#include <allansm/io.hpp>
 
 main(int argc,char** argv){
-	HWND previous = NULL;
+	Win win;
+	Time timeh;
 	float opacity = 0.8;
 	
 	if(argc == 2){
-		opacity = unknown(argv[1]);
+		opacity = std::atof(argv[1]);
 	}
 
+	float screen = win.getSize(GetDesktopWindow())[0]*win.getSize(GetDesktopWindow())[1];
+	
 	while(true){
-		if(previous != NULL){
-			Win().changeOpacity(previous, 1);
-		}
+		auto focused = win.getFocused();
+		auto fname = win.getAppname(focused);
+		auto fsize = win.getSize(focused);
+		float percent = (100/screen)*(fsize[0]*fsize[1]);
+		float x = opacity/2;
 		
-		auto focused = Win().getFocused();
-		Win().changeOpacity(focused, unknown(opacity));
+		win.changeOpacity(focused, opacity);
+		
+		print(fname+" ");
+		println(percent);
 
-		previous = focused;
+		if(percent >= 80) x = 0.0;
+		
+		for(auto n : win.hOnDisplay()){
+			if(focused != n && win.getAppname(n) != "explorer"){
+				win.changeOpacity(n, x);
+			}
 
-		Time().sleep(1000);
+			timeh.sleep(1);
+		}
+
+		timeh.sleep(1000);
 	}
 }
