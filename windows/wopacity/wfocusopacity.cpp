1be8d4f760bf7e5b0b9da44ed441afb77e96e034
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2022-07-10 16:23:29 -0300

    Ignore executables by name

--- windows/wopacity/wfocusopacity.cpp
+++ windows/wopacity/wfocusopacity.cpp
@@ -1,12 +1,30 @@
 #include <allansm/win.hpp>
 #include <allansm/time.hpp>
 #include <allansm/io.hpp>
+#include <allansm/file.hpp>
+
+bool ignored(std::string name, std::vector<std::string> ignore){
+	Time t;
+
+	for(auto n : ignore){
+		if(n == name) return true;
+		
+		t.sleep(1);
+	}
+
+	return false;
+}
 
 main(int argc,char** argv){
 	Win win;
 	Time timeh;
 	float opacity = 0.8;
 	auto prev = win.getFocused();
+	File config(".config");
+
+	std::vector<std::string> ignore;
+
+	if(config.exists()) ignore = config.lines();
 
 	if(argc == 2){
 		opacity = std::atof(argv[1]);
@@ -21,17 +39,23 @@ main(int argc,char** argv){
 		float percent = (100/screen)*(fsize[0]*fsize[1]);
 		float x = opacity/2;
 		
-		if(percent >= 25) win.changeOpacity(focused, opacity);
-		else win.changeOpacity(focused, 1);
+		if(!ignored(fname, ignore)){
+			if(percent >= 25) win.changeOpacity(focused, opacity);
+			else win.changeOpacity(focused, 1);
 
-		print(fname+" ");
-		println(percent);
+			print(fname+" ");
+			println(percent);
+		}else{
+			println(fname+" ignored");
+		}
 
 		if(focused != prev){
 			if(percent >= 80) x = 0.0;
 			
 			for(auto n : win.hOnDisplay()){
-				if(focused != n && win.getAppname(n) != "explorer"){
+				auto name = win.getAppname(n);
+
+				if(focused != n && name != "explorer" && !ignored(name, ignore)){
 					if(!win.isTopmost(n))
 						win.changeOpacity(n, x);
 				}
